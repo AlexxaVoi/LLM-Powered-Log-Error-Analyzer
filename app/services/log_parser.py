a0f3df116fa37c8e67001127bc2dbcdf2edbd846
AlexxaVoi <voinovasasa@gmail.com>
2026-03-11 21:53:44 +0200

    fix: log parser

--- app/services/log_parser.py
+++ app/services/log_parser.py
@@ -7,9 +7,10 @@ def file_transformation(file_content: bytes) -> str:
 
 
 def clean_log(full_text: str) -> str:
-    df = pd.DataFrame(full_text.splitlines(), columns=["lof_info"])
-    keywords = "ERROR|CRITICAL|EXCEPTION|FAILED"
-    filtered_df = df[df["lof_info"].str.contains(keywords, case=False, na=False)]
+    df = pd.DataFrame(full_text.splitlines(), columns=["log_info"])
+    keywords = r"\b(?:ERROR|CRITICAL|EXCEPTION|FAILED)\b"
+
+    filtered_df = df[df["log_info"].str.contains(keywords, case=False, na=False)]
     clean_text = "\n".join(filtered_df["log_info"].to_list())
 
     if clean_text:
